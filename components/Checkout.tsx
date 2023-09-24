36184ef0c5bd75625a18e6682bdc43d65a1d3299
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2023-09-24 08:58:59 +0500

    v0.37
    *Checkout Updated

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -1,32 +1,13 @@
 "use client";
-import Image from "next/image";
 import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 //
-import books from "@lib/Books";
+import Item from "@components/Item";
 import { type CartItem } from "@lib/Interface";
-
-// Items
-const items: CartItem[] =
-  [
-    {
-      title: books[0].title,
-      price: books[0].price,
-      image: books[0].image,
-      quantity: 1
-    },
-    {
-      title: books[1].title,
-      price: books[1].price,
-      image: books[1].image,
-      quantity: 3
-    }
-  ];
 
 // Checkout
 export default function Checkout(): JSX.Element
 {
+  const items: CartItem[] = [];
   const [total, setTotal] = useState<number>(0);
 
   // On Mount
@@ -76,40 +57,6 @@ export default function Checkout(): JSX.Element
           </div>
         }
       </div >
-    </>
-  );
-}
-
-// Item
-function Item({ title, price, image, quantity }: CartItem): JSX.Element
-{
-  return (
-    <>
-      <div className=" w-full my-4 col-span-6 md:col-span-8 flex justify-center items-center">
-        <Image
-          src={ image }
-          alt={ title }
-          draggable="false"
-          className=" w-12 md:w-20 mr-1 rounded"
-        />
-        <div className=" w-full ml-1">
-          <h2 className=" w-full mb-1 text-sm md:text-base font-medium font-secondary"> { title } </h2>
-          <h3 className=" w-full mt-1 text-xs md:text-sm font-primary"> { `Rs ${ price }` } </h3>
-        </div>
-      </div>
-
-      <div className=" w-full my-4 col-span-3 md:col-span-2 flex justify-center items-center">
-        <h3 className=" w-10 md:w-20 h-10 mr-1 flex justify-center items-center rounded text-sm md:text-base font-primary bg-white"> { quantity } </h3>
-        <button className=" w-10 h-10 ml-1 rounded hover:text-white bg-white hover:bg-dark-primary scale">
-          <FontAwesomeIcon
-            icon={ faTrashCan }
-          />
-        </button>
-      </div>
-
-      <div className=" w-full my-4 col-span-3 md:col-span-2 flex justify-center items-center">
-        <h3 className=" w-full text-end text-sm md:text-base font-primary"> { `Rs ${ price * quantity }` } </h3>
-      </div>
     </>
   );
 }
